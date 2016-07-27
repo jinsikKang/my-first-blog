5268486e8cd1b5cf9003592ea45d8cda476b0780
jsKang <jskang@chironsoft.com>
2016-07-27 17:01:27 +0900

    Modified ajax, templates to display posts from database.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^add_post/$', views.write_post, name='write_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 #_*_coding:utf-8_*_
+
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
@@ -6,3 +8,11 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def write_post(request):
+    me = User.objects.get(username='jskang')
+    title = request.GET['title' ]
+    text  = request.GET['text'  ]
+    post = Post.objects.create(author=me, title=title, text=text)
+    post.publish()
+    return render(request, 'blog/post_list.html', {})
